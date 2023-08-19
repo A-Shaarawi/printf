54641db587718afd971e32b95d3de7b93a5c62ce
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-19 04:23:31 -0700

    updating..

--- functions.c
+++ functions.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
-/***/
-
+/**
+ * print_char - returns char given in argument
+ * @va_list: pointer to the list of arguments of _printf variadic function
+ * Return: the character to be printed
+*/
 char print_char(va_list)
 {
-	//char c = va_arg(args, int);
-
-	int v
-	while ((v = va_arg(args, int))
-		if ()
+	char c = va_arg(va_list, int);
 	return (c);
 }
 
+/**
+ * print_string - return string given in argument
+ * @va_list: pointer to the list of arguments of _printf variadic function
+ * Return: the string to be printed
+*/
 char *print_str(va_list)
 {
+	char *str = va_arg(va_list, char *);
+	return(str);
 }
