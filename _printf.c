5fd9e569f00322fd24ce4f16d7c223c86d5ead0b
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-19 00:34:25 -0700

    update

--- _printf.c
+++ _printf.c
@@ -8,16 +8,11 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-
-	int j = 0;
-
+	int i = 0, j = 0;
 	va_list args;
-
 	char printed[];
 
 	va_start(args, format);
-	
 	for (;format[i] != '\0'; i++)
 	{
 		if (format && format[i] == '%')
