bcff21f90ed481f66d5a30866784056a308786b0
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-08-19 10:18:12 -0700

    printf-A

--- _printf.c
+++ _printf.c
@@ -1,17 +1,20 @@
 #include "main.h"
 #include <string.h>
 #include <stdarg.h>
-/*
- * _printf - fucntion that prints
+/**
+ * _printf - function that prints
+ *
  * @format: parameter
+ * @...: parameter
  * Return: 0
  */
+
 int _printf(const char *format, ...)
 {
 	int i = 0;
 
 	va_list args;
-	
+
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
@@ -20,22 +23,24 @@ int _printf(const char *format, ...)
 			if (format[i + 1] == 'c')
 			{
 				char c = va_arg(args, int);
+
 				write(fd, &c, 1);
 			}
 			else if (format[i + 1] == 's')
 			{
 				char *str = va_arg(args, char *);
-				write(fd,str,strlen(str));
+
+				write(fd, str, strlen(str));
 			}
 			else if (format[i + 1] == '%')
 			{
-				write(1,"%%",1);
+				write(1, "%%", 1);
 			}
 			i++;
 		}
 		else if (format[i] != '%')
 		{
-			write(fd,&format[i],1);
+			write(fd, &format[i], 1);
 		}
 	}
 	va_end(args);
