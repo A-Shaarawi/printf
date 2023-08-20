e16fb2f3ad5c4e456901cec8c75d2eb7cd1a2638
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-20 05:46:40 -0700

    updating

--- _printf.c
+++ _printf.c
@@ -30,20 +30,16 @@ int _printf(const char *format, ...)
 			else if (format[i + 1] == 's')
 			{
 				str = va_arg(args, char *);
-				if (str == NULL)
-				{
-					write(1, "(null)", 6);
-					return (6);
-				}
-				else
-					write(fd, str, strlen(str));
+				write(fd, str, strlen(str));
 			}
 			else if (format[i + 1] == '%')
 			{
 				write(1, "%%", 1);
 			}
-			else
-				return (-1);
+			else if (format[i + 1] == ' ')
+				return (255);
+			else if (format[i + 1] == '\0')
+				return (255);
 			i++;
 		}
 		else if (format[i] != '%')
