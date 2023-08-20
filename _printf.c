c6b94fcf5ef6720e48d5dfdedfa6600dc4ea87ed
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-08-20 09:18:50 -0700

    printf-A

--- _printf.c
+++ _printf.c
@@ -20,8 +20,6 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	if (format == '\0')
-		return (1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
