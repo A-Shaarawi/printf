e85c6c83f695fc20c1857b542e6ec67294775693
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-08-21 09:47:02 -0700

    printf-A

--- _printf.c
+++ _printf.c
@@ -10,55 +10,7 @@ int _printf(const char *format, ...)
         char *str, c;
         va_list args;
 
-        va_start(args, format);
-        if (format == NULL)
-                return (-1);
-        for (; format[i] != '\0'; i++)
-        {
-                if (format[i] == '%')
-                {
-                        if (format[i + 1] == 'c')
-                        {
-                                c = (char)va_arg(args, int);
-                                write(fd, &c, 1), j++, i++;
-                        }
-                        else if (format[i + 1] == 's')
-                        {
-                                str = va_arg(args, char *);
-                                if (str == NULL)
-                                        write(1, "(null)", 6), i++, j += 6;
-                                else
-                                write(fd, str, _strlen(str)), j += _strlen(str), i++;
-                        }
-                        else if (format[i + 1] == 'd')
-                        {
-                                i++;
-                                int num = va_arg(args, int);
-                                str = _sprintf(num);
-                                write(1, str, _strlen(str));
-                        }
-                        else if (format[i + 1] == 'i')
-                        {
-                                i++;
-                                char *str2;
-                                int num2 = va_arg(args, int);
-                                str2 = _sprintf(num2);
-                                write(1, str2, _strlen(str2));
-                        }
-                        else if (format[i + 1] == '%')
-                                write(1, "%%", 1), j++, i++;
-                        else if (format[i + 1] == '\0')
-                                return (-1);
-                        else if (format[i + 1] != 's' && format[i + 1] != 'c' &&
-                                        format[i + 1] != '%')
-                                write(fd, &(format[i]), 1), j++;
-                        else
-                                return (-1);
-                }
-                else if (format[i] != '%')
-                        write(fd, &(format[i]), 1), j++;
-        }
-        va_end(args);
-        return (j);
-
-}
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	for (; format[i] 
