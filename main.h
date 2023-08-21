1226f1b0ea5e3cc0f0b59841099078c9621ae029
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-21 09:19:46 -0700

    updates

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
+#include <math.h>
 #define fd STDOUT_FILENO
 
 
--- more_func.c
+++ more_func.c
@@ -13,3 +13,21 @@ int _strlen(char *str)
 
 	return (i);
 }
+
+/***/
+char *_sprintf(int num)
+{
+	int i = 0, len, t;
+	char str[32], ch;
+
+	len = _strlen(num);
+	for (; num != 0; i++)
+	{
+		t = pow(10, len - i - 1);
+		ch = (num / t) + 48;
+		num %= t;
+		str[i] = 'ch'
+	}
+	str[i] = '\0';
+	return (str);
+}
