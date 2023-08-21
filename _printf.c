5ece1fbe0bb0dbb36e159d65adad0abfb99228ef
A-Shaarawi <abdelrahmangamal305@gmail.com>
2023-08-21 09:44:30 -0700

    printf-A

--- _printf.c
+++ _printf.c
@@ -49,7 +49,7 @@ int _printf(const char *format, ...)
                                 write(1, "%%", 1), j++, i++;
                         else if (format[i + 1] == '\0')
                                 return (-1);
-                        else if (format[i + 1] != 's' || format[i + 1] == 'c' ||
+                        else if (format[i + 1] != 's' && format[i + 1] != 'c' &&
                                         format[i + 1] != '%')
                                 write(fd, &(format[i]), 1), j++;
                         else
